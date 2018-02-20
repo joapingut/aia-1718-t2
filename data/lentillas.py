20afab01cf55afe5c98ac6bc728dd2d19e957030
joapingut <joapingut@mail.com>
2018-02-20 20:18:41 +0100

    Rules algorimth parcially implemented

---
+++ data/lentillas.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+clasificacion='Lentillas'
+
+# Valores de clasificación
+
+clases=['rigida','blanda', 'ninguna']
+
+atributos=[('edad',["joven","prePresbicia","presbicia"]),
+           ('diagnostico',["miope","hiper"]),
+           ('astigmatismo',["s","n"]),
+           ('lagrima',["normal","reducida"])]
+
+entrenamiento=[['joven', 'miope', 'n', 'reducida', 'ninguna'],
+               ['joven', 'miope', 'n', 'normal', 'blanda'],
+               ['joven', 'miope', 's', 'reducida', 'ninguna'],
+               ['joven', 'miope', 's', 'normal', 'rigida'],
+               ['joven', 'hiper', 'n', 'reducida', 'ninguna'],
+               ['joven', 'hiper', 'n', 'normal', 'blanda'],
+               ['joven', 'hiper', 's', 'reducida', 'ninguna'],
+               ['joven', 'hiper', 's', 'normal', 'rigida'],
+               ['prePresbicia', 'miope', 'n', 'reducida', 'ninguna'],
+               ['prePresbicia', 'miope', 'n', 'normal', 'blanda'],
+               ['prePresbicia', 'miope', 's', 'reducida', 'ninguna'],
+               ['prePresbicia', 'miope', 's', 'normal', 'rigida'],
+               ['prePresbicia', 'hiper', 'n', 'reducida', 'ninguna'],
+               ['prePresbicia', 'hiper', 'n', 'normal', 'blanda'],
+               ['prePresbicia', 'hiper', 's', 'reducida', 'ninguna'],
+               ['prePresbicia', 'hiper', 's', 'normal', 'ninguna'],
+               ['presbicia', 'miope', 'n', 'reducida', 'ninguna'],
+               ['presbicia', 'miope', 'n', 'normal', 'ninguna'],
+               ['presbicia', 'miope', 's', 'reducida', 'ninguna'],
+               ['presbicia', 'miope', 's', 'normal', 'rigida'],
+               ['presbicia', 'hiper', 'n', 'reducida', 'ninguna'],
+               ['presbicia', 'hiper', 'n', 'normal', 'blanda'],
+               ['presbicia', 'hiper', 's', 'reducida', 'ninguna'],
+               ['presbicia', 'hiper', 's', 'normal', 'ninguna']]
--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from clasificadores.clasificador import *
 from clasificadores.clasificadorDT import *
 from clasificadores.clasificadorDTPoda import *
 
-import data
+import data.votos as Votos
+
+import data.lentillas as Lentillas
+
+import clasificadores.clasificadorDR as ClasifDR
 
 ''' Ejecutar este archivo para hacer las pruebas '''
+
+for regla in ClasifDR.entrenador(Lentillas.entrenamiento, Lentillas.atributos):
+    print(regla)
